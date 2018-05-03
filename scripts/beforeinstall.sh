ec67fd5c176d6d8f9e6ce3743c9723ed620eab1f
Alex Varela <shion093@gmail.com>
2018-05-03 00:48:39 -0600

    node

--- scripts/beforeinstall.sh
+++ scripts/beforeinstall.sh
@@ -1,5 +1,5 @@
 apt-get update
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
-sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_9.x | -E bash -
+apt-get install -y nodejs
 npm install -g pm2
 pm2 update

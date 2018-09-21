c9bb90500206112714a5178431a95059d02a081d
Alex Varela <shion093@gmail.com>
2018-09-21 01:00:12 -0600

    change password actions

--- server/api/users/actions.js
+++ server/api/users/actions.js
@@ -13,10 +13,10 @@ async function createUser (req, res, next) {
     const verificationCode = Math.floor(1000 + Math.random() * 9000);
 
     const params = {
-      message: `Hello breee, su codigo de verificacion es: ${verificationCode}`,
-      phone: req.body.phoneNumber,
+      message : `Hello breee, su codigo de verificacion es: ${verificationCode}`,
+      phone   : req.body.phoneNumber,
     };
-    
+
     // Create promise and SNS service object
     await sendSMS(params);
     req.body.isActive = false;
@@ -45,7 +45,7 @@ async function test (req, res, next) {
 async function validateEmail (req, res, next) {
   try {
     const user = await User.findOne({ email : req.params.email });
-    return res.status(200).json({exist: user !== null });
+    return res.status(200).json({ exist : user !== null });
   } catch (err) {
     return errorHandler(err, req, res, next);
   }
@@ -54,13 +54,72 @@ async function validateEmail (req, res, next) {
 async function verifyPhone (req, res, next) {
   try {
     const user = await User.findOne({ email : req.user.email });
-    if ( user.verificationCode === req.body.code) {
+    if (user.verificationCode === req.body.code) {
       user.isActive = true;
       await user.save();
       return res.status(200).json({ confirmed : true });
     }
     return res.status(200).json({ confirmed : false });
-  } catch(err) {
+  } catch (err) {
+    return errorHandler(err, req, res, next);
+  }
+}
+
+async function changePassword (req, res, next) {
+  try {
+    const user = await User.findOne({ email : req.user.email });
+    console.log(user);
+    const isMatch = await user.comparePassword(req.body.password);
+    if (user && !isMatch) {
+      user.password = req.body.password;
+      await user.save();
+      return res.status(200).json({ changed : true });
+    }
+    return res.status(200).json({ changed : false });
+  } catch (err) {
+    return errorHandler(err, req, res, next);
+  }
+}
+
+async function verifyPhonePass (req, res, next) {
+  try {
+    const user = await User.findOne({
+      email            : req.body.email,
+      verificationCode : req.body.code,
+    });
+    if (user) {
+      user.isActive = true;
+      await user.save();
+      const { token } = await user.generateToken(user);
+      return res.status(200).json({ password : true, token });
+    }
+    return res.status(200).json({ password : false });
+  } catch (err) {
+    return errorHandler(err, req, res, next);
+  }
+}
+
+async function forgotPassword (req, res, next) {
+  try {
+    const user = await User.findOne({ email : req.body.email });
+
+    if (user) {
+      const verificationCode = Math.floor(1000 + Math.random() * 9000);
+
+      const params = {
+        message : `Hello breee, su codigo de verificacion es: ${verificationCode}`,
+        phone   : user.phoneNumber,
+      };
+
+      // Create promise and SNS service object
+      await sendSMS(params);
+      // user.isActive = false;
+      user.verificationCode = verificationCode.toString();
+      await user.save();
+      return res.status(200).json({ sent : true });
+    }
+    return res.status(200).json({ sent : false });
+  } catch (err) {
     return errorHandler(err, req, res, next);
   }
 }
@@ -70,4 +129,7 @@ module.exports = {
   test,
   validateEmail,
   verifyPhone,
+  forgotPassword,
+  verifyPhonePass,
+  changePassword,
 };

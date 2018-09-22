a2703cbf7a5e85b384cda1fa2d916d5e7ea3c2b9
Alex Varela <shion093@gmail.com>
2018-09-22 02:11:39 -0600

    jwt fixed

--- server/config/express.js
+++ server/config/express.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(compress());
 app.use(helmet());
 app.use(cors());
-// initialize();
+initialize();
 setJwtStrategy();
 
 app.use('/v1', routes);
